5c0dcc128fa58ad6077f6d64ae1b70537a666714
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-05-13 10:11:27 +0200

    Use nested CLI
    @bgyori note that I split the custom downloader into its own subcommand

--- src/umls_downloader/cli.py
+++ src/umls_downloader/cli.py
@@ -14,6 +14,7 @@ later, but that will cause problems--the code will get executed twice:
 """
 
 import logging
+from pathlib import Path
 from typing import Optional
 
 import click
@@ -30,29 +31,60 @@ __all__ = [
 
 logger = logging.getLogger(__name__)
 
-
-@click.command()
-@verbose_option
-@click.option(
-    "--url", help="The URL for a file to be downloaded through the UMLS ticket granting system."
-)
-@click.option("--output", help="The local file path to download a file to if using --url")
-@click.option(
+api_option = click.option(
     "--api-key",
     help="The API key for the UMLS ticket granting system. If not given, uses pystow to load."
-    " Get one at https://uts.nlm.nih.gov/uts/edit-profile. ",
+    " Get one at https://uts.nlm.nih.gov/uts/edit-profile.",
 )
-@force_option
-def main(url: Optional[str], output: Optional[str], api_key: Optional[str], force: bool):
-    """Download the given version of the UMLS or another UMLS-controlled resource via a custom URL."""
-    if url and output:
-        download_tgt(url=url, path=output, api_key=api_key, force=force)
-    else:
-        umls_path = download_umls(api_key=api_key, force=force)
-        click.secho(f"Downloaded UMLS to {umls_path}")
 
-        rxnorm_path = download_rxnorm(api_key=api_key, force=force)
-        click.secho(f"Downloaded RxNorm to {rxnorm_path}")
+version_option = click.option(
+    "--version",
+    help="The version to download. If none specified, looks up the latest with bioversions",
+)
+
+
+@click.group()
+def main():
+    """Download files from the UMLS Terminology Service."""
+
+
+@main.command()
+@verbose_option
+@api_option
+@force_option
+@click.option(
+    "--url",
+    help="The URL for a file to be downloaded through the UMLS ticket granting system.",
+    required=True,
+)
+@click.option("-o", "--output", help="The local file path to download a file to", required=True)
+def custom(url: str, output: str, api_key: Optional[str], force: bool):
+    """Download a file via a custom URL."""
+    path = Path(output).expanduser().resolve()
+    download_tgt(url=url, path=path, api_key=api_key, force=force)
+    click.secho(str(path))
+
+
+@main.command()
+@verbose_option
+@version_option
+@force_option
+@api_option
+def umls(version: Optional[str], force: bool, api_key: Optional[str]):
+    """Download the UMLS data and print the path to stdout."""
+    path = download_umls(api_key=api_key, force=force, version=version)
+    click.secho(str(path))
+
+
+@main.command()
+@verbose_option
+@version_option
+@force_option
+@api_option
+def rxnorm(version: Optional[str], force: bool, api_key: Optional[str]):
+    """Download the RxNorm data and print the path to stdout."""
+    path = download_rxnorm(api_key=api_key, force=force, version=version)
+    click.secho(str(path))
 
 
 if __name__ == "__main__":
